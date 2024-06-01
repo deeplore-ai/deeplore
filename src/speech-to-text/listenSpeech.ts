c3738d143e09e5d255edd04725825f7d09f09f73
lepzulnag <gindre.matthieu@gmail.com>
2024-06-01 22:04:32 +0200

    Remove keywords

--- src/speech-to-text/listenSpeech.ts
+++ src/speech-to-text/listenSpeech.ts
@@ -1,7 +1,7 @@
 import { speechObserver } from "../observables/speechObserver";
 
 const socket = new WebSocket(
-  "wss://api.deepgram.com/v1/listen?language=fr&keywords=Emma&keywords=Dubois&search=Emma%20Dubois",
+  "wss://api.deepgram.com/v1/listen?language=fr&search=Emma%20Dubois",
   ["token", "4cebb2572a5a31e8780f478ee7933df45c45a63d"]
 );
 
