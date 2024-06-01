8b7a2d0a77b926b712c05512300b7726356f4a9f
Lucas ROURET <lucas.rouretperso@gmail.com>
2024-06-01 21:57:13 +0200

    prompt

--- agent-AI/app/gemini.py
+++ agent-AI/app/gemini.py
@@ -22,7 +22,11 @@ genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel(model_name='gemini-pro')
 
 def chat_gemini(speech: Speech):
-    chat_response = model.generate_content(getPrompt(speech))  
+    chat_response = model.generate_content(f"""
+                           {getPrompt(speech)} \n
+                            {speech.speaker} is speaking and says : "{speech.content}" \n
+                            Respond in french if needed. I only want the answer.
+                            """)  
     if DEBUG :
         print(chat_response)
     return chat_response.text
