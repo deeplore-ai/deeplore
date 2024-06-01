7e4ecb1d8dcb1af12b844b937e0690f49811b101
Lucas Rouret <lucasro@bam.tech>
2024-06-01 17:53:23 +0200

    feat: add echap in chat

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -5,7 +5,7 @@ import { PlayerDirection } from "../types";
 import Character from "../models/Character";
 import * as easystarjs from "easystarjs";
 import { calculateDistance, fromXYToGrid } from "../utils";
-import { canvas, chatButton, isUIOpen, openUI } from "../lib/UI";
+import { canvas, chatButton, closeUI, isUIOpen, openUI } from "../lib/UI";
 import type jsonMap from "../../public/map.json";
 
 type Map = typeof jsonMap;
@@ -14,9 +14,33 @@ const easystar = new easystarjs.js();
 import Game from "../models/Game";
 
 const characters = [
-  new Character("char1", k.vec2(1343, 1052), 250, scaleFactor, k, "Paul", "Martinez"),
-  new Character("Girl", k.vec2(1343, 1400), 250, scaleFactor, k, "Emma", "Dubois"),
-  new Character("Priest", k.vec2(1250, 900), 250, scaleFactor, k, "Matthieu", "Mancini"),
+  new Character(
+    "char1",
+    k.vec2(1343, 1052),
+    250,
+    scaleFactor,
+    k,
+    "Paul",
+    "Martinez"
+  ),
+  new Character(
+    "Girl",
+    k.vec2(1343, 1400),
+    250,
+    scaleFactor,
+    k,
+    "Emma",
+    "Dubois"
+  ),
+  new Character(
+    "Priest",
+    k.vec2(1250, 900),
+    250,
+    scaleFactor,
+    k,
+    "Matthieu",
+    "Mancini"
+  ),
 ];
 
 export const createMainScene = () => {
@@ -24,6 +48,13 @@ export const createMainScene = () => {
     canvas.addEventListener("keydown", (e) => {
       if (e.key === "Enter" && !isUIOpen()) {
         openUI(onPlayerAskQuestion);
+      }
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" && isUIOpen()) {
+        canvas.focus();
+        closeUI();
       }
     });
 
@@ -85,7 +116,6 @@ export const createMainScene = () => {
       });
     }
 
-
     // characters[2].setTarget(characters[0].gameObject.pos);
     // recalculatePath(characters[2]);
     /*     openUI((textInput) => {
@@ -145,7 +175,7 @@ function fromGridToDirection(
   }
 }
 
-function recalculatePath(character: Character) {
+const recalculatePath = (character: Character) => {
   if (!character.target) return;
   const charGridPos = fromXYToGrid(
     character.gameObject.pos.x,
@@ -173,11 +203,17 @@ function recalculatePath(character: Character) {
     }
   );
   easystar.calculate();
-}
+};
 
 function onPlayerAskQuestion(textInput: string) {
-  const priestDistance = calculateDistance(characters[0].gameObject.pos, characters[2].gameObject.pos);
-  const emmaDistance = calculateDistance(characters[0].gameObject.pos, characters[1].gameObject.pos);
+  const priestDistance = calculateDistance(
+    characters[0].gameObject.pos,
+    characters[2].gameObject.pos
+  );
+  const emmaDistance = calculateDistance(
+    characters[0].gameObject.pos,
+    characters[1].gameObject.pos
+  );
   if (priestDistance < LISTEN_RANGE) {
     characters[2].hear(textInput, "Paul Martinez");
   }
