10671bfc869296fa9a7506f7de8550f4affd8b29
CartierPierre <heraknoshsdj@hotmail.fr>
2024-06-01 13:58:25 +0200

    test mistral ok

--- agent-AI/app/mistral.py
+++ agent-AI/app/mistral.py
@@ -4,3 +4,32 @@ import yaml
 
 api_key = yaml.safe_load(open("/.env.yaml", "r"))["MISTRAL_API_KEY"]
 model = "mistral-large-latest"
+
+client = MistralClient(api_key=api_key)
+
+test_policeman_diag = "Hello, I am a policeman, I am investigating on a murder in the town. Did you see or hear something strange ?"
+
+chat_response = client.chat(
+    model=model,
+    messages=[ChatMessage(role="user", content=f"""
+                          Respond in french. 
+                          You are a non playable character in a murder story game. 
+                          You are the priest of the town and you saw at strange people last night running in the streets with a knife. 
+                          You respond to the policeman who says : "{test_policeman_diag}" """)]
+)
+
+print(chat_response.choices[0].message.content)
+
+def test_chat():
+    chat_response = client.chat(
+    model=model,
+    messages=[ChatMessage(role="user", content=f"""
+                          Respond in french. 
+                          You are a non playable character in a murder story game. 
+                          You are the priest of the town and you saw at strange people last night running in the streets with a knife. 
+                          You respond to the policeman who says : "{test_policeman_diag}" """)]
+)
+
+    
+    
+    
