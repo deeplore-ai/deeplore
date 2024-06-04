0536373dd61c1840b26009840977c2c4caac2538
Josselin TILLAY <j.tillay@smartway.ai>
2024-06-04 15:48:20 +0200

    Move folders

--- src/lib/ctx.ts
+++
@@ -1,8 +0,0 @@
-import kaboom from "kaboom";
-
-export const k = kaboom({
-  global: false,
-  touchToMouse: true,
-  canvas: document.getElementById("game") as HTMLCanvasElement,
-  debug: true,
-});
