9ce92768e024b1bd5d0ec676b623c9667dc1009a
Josselin TILLAY <j.tillay@smartway.ai>
2024-06-05 17:47:14 +0200

    Add openai endpoint

--- server/app/config.py
+++ server/app/config.py
@@ -1,8 +1,14 @@
+import os
 import yaml
 
 # Load env variables from .env.yaml file
-MISTRAL_API_KEY = yaml.safe_load(open("/.env.yaml", "r"))["MISTRAL_API_KEY"]
-GOOGLE_API_KEY = yaml.safe_load(open("/.env.yaml", "r"))["GOOGLE_API_KEY"]
+MISTRAL_API_KEY = yaml.safe_load(open(".env.yaml", "r"))["MISTRAL_API_KEY"]
+GOOGLE_API_KEY = yaml.safe_load(open(".env.yaml", "r"))["GOOGLE_API_KEY"]
+OPENAI_API_KEY = yaml.safe_load(open(".env.yaml", "r"))["OPENAI_API_KEY"]
+
+os.environ["MISTRAL_API_KEY"] = MISTRAL_API_KEY
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 # DEBUG flag
-DEBUG=True
+DEBUG = True
